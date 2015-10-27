228ae79379952aaa4247c9ade172dca723b42cc9
Michael Hansen <michael@han5en.org>
2015-10-26 23:08:26 -0600

    new wifi cmd wrapper

---
+++ bin/wifi_cmd.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+if($ARGV[0] eq "start")
+{
+  #print "starting wifi...\n";
+  `/usr/bin/logger "starting wifi..."`;
+  `/sbin/ifup wlan0 > /dev/null 2>&1`;
+}
+elsif($ARGV[0] eq "stop")
+{
+  #print "stopping wifi...\n";
+  `/usr/bin/logger "stopping wifi..."`;
+  `/sbin/ifdown --force wlan0`;
+}
+elsif($ARGV[0] eq "check")
+{
+  $ping = `/bin/ping -c4 192.168.2.1`;
+  #print "$ping\n";
+
+  if($ping =~ /100% packet loss/) {
+    #print "restarting wifi...\n";
+    `/usr/bin/logger "restarting wifi..."`;
+    `/sbin/ifdown --force wlan0`;
+    sleep 10;
+    `/sbin/ifup wlan0 > /dev/null 2>&1`;
+  }
+}
+else
+{
+  die "bad cmd: $ARGV[0]";
+}
+

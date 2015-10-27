56a9be92ffb547cc249a62642956bce2cd00ec08
Michael Hansen <michael@han5en.org>
2015-10-27 12:56:14 -0600

    refining wifi_Cmd

--- bin/wifi_cmd.pl
+++ bin/wifi_cmd.pl
@@ -16,19 +16,43 @@ elsif($ARGV[0] eq "stop")
 }
 elsif($ARGV[0] eq "check")
 {
-  $ping = `/bin/ping -c4 $gw`;
-  #print "$ping\n";
-
-  if($ping =~ /100% packet loss/) {
+  unless(&ping()) {
     #print "restarting wifi...\n";
     `/usr/bin/logger "restarting wifi..."`;
     `/sbin/ifdown --force wlan0`;
     sleep 10;
     `/sbin/ifup wlan0 > /dev/null 2>&1`;
   }
+
+  ## if that didn't work... might just need to reboot...
+  unless(&ping()) {
+    #print "rebooting...\n";
+    `/usr/bin/logger -t wifi_cmd "rebooting..."`;
+    sleep 10;
+    `/sbin/shutdown -r now`;
+  }
 }
 else
 {
   die "bad cmd: $ARGV[0]";
+}
+
+exit;
+
+#
+#
+#
+
+sub ping
+{
+  my $ping = `/bin/ping -c4 -w1 $gw`;
+  #print "$ping\n";
+
+  if($ping =~ /1 received/) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
 }
 

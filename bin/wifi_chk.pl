834b4b6fcac6c12a40846265845de44bacc81f20
Michael Hansen <michael@han5en.org>
2015-10-26 22:50:29 -0600

    new wifi check

--- bin/wifi_chk.pl
+++ bin/wifi_chk.pl
@@ -6,8 +6,8 @@ $ping = `/bin/ping -c4 192.168.2.1`;
 if($ping =~ /100% packet loss/) {
   print "restarting wifi...\n";
   `/usr/bin/logger "restarting wifi..."`;
-  `/sbin/ifdown --force wlan0 > /dev/null`;
+  `/sbin/ifdown --force wlan0`;
   sleep 10;
-  `/sbin/ifup wlan0 > /dev/null`;
+  `/sbin/ifup wlan0 > /dev/null 2>&1`;
 }
 

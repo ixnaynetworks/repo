6ed1a031c2b9d005ceec252655962efa406b71e7
Michael Hansen <michael@han5en.org>
2015-11-24 00:08:01 -0700

    new graphing stuff

---
+++ bin/mp2.pl
@@ -0,0 +1,175 @@
+#!/usr/bin/perl
+
+$| = 1;
+
+#require '/home/pi/bin/ppp.pl';
+
+$upload = ARGV[0];
+
+$time = time();
+
+#
+# open output file, or STDOUT if none is specified
+#
+
+my($sec, $min, $hr, $day, $mon, $year) = localtime($time);
+my $year += 1900;
+my $mon++;
+my $file = sprintf("%4d%02d%02d%02d%02d%02d.txt", $year, $mon, $day, $hr, $min, $sec);
+
+open(TXT, ">", "/var/www/graphs2/status_pi/$file");
+
+#
+# run some commands
+#
+
+my($cmd, $out);
+
+#
+# date
+#
+
+$cmd = "/bin/date +\"%Y/%m/%d %T\"";
+my $date = `$cmd`;
+print TXT "\n$cmd\n", $date;
+
+## for later use...
+chomp($date);
+
+#
+# uptime
+#
+
+$cmd = "/usr/bin/uptime";
+$out = `$cmd`;
+print TXT "\n##########\n\n", "$cmd\n", $out;
+
+#
+# traceroute
+#
+
+$cmd = "/usr/bin/sudo /usr/sbin/traceroute -T -n 54.69.208.7";
+$out = `$cmd`;
+print TXT "\n##########\n\n", "$cmd\n", $out;
+
+my($sum, $num, $net);
+if($out =~ /\n[\s\d].*54.69.208.7(.*)/s) {
+  foreach (split(/\s+/, $1)) {
+    if(/^[\d\.]+$/) {
+      $sum += $_;
+      $num++;
+    }
+  }
+  $net = $sum / $num;
+}
+
+&record_dat("/var/www/html/graphs2/dat/net.txt", $net);
+
+#
+# temp
+#
+
+$cmd = "/opt/vc/bin/vcgencmd measure_temp";
+$out = `$cmd`;
+
+my $temp;
+if($out =~ /^temp=(.*)'C/) {
+  $temp = sprintf "%.1f", (($1 * 9) / 5) + 32;
+  $out =~ s/^temp=.*'C/temp=$temp\'F/;
+}
+
+print TXT "\n##########\n\n", "$cmd\n", $out;
+
+&record_dat("/var/www/html/graphs2/dat/temp.txt", $temp);
+
+#
+# mopicli
+#
+
+#$cmd = "/usr/sbin/mopicli -e";
+#$out = `$cmd`;
+#print $sh "\n##########\n\n", "$cmd\n", $out;
+
+#
+# df
+#
+
+$cmd = "/bin/df -h";
+$out = `$cmd`;
+print TXT "\n##########\n\n", "$cmd\n", $out;
+
+my $disk;
+if($out =~ /dev\/root.* (\d\d?)\%/) {
+  $disk = $1;
+}
+
+&record_dat("/var/www/html/graphs2/dat/disk.txt", $disk);
+
+#
+# top
+#
+
+$cmd = "/usr/bin/top -b -n1 | /usr/bin/head -30";
+$out = `$cmd`;
+print TXT "\n##########\n\n", "$cmd\n", $out;
+
+#
+# upload
+#
+
+if($upload)
+{
+  my($name) = `/bin/hostname`;
+  chomp($name);
+
+  $base = "www/vhosts/ixnay/htdocs/cams/$name";
+
+  my $cmd;
+
+  $cmd  = "/usr/bin/scp";
+  $cmd .= " /var/www/html/graphs2/status_pi.txt";
+  $cmd .= " /var/www/html/graphs2/net.txt";
+  $cmd .= " /var/www/html/graphs2/temp.txt";
+  $cmd .= " /var/www/html/graphs2/disk.txt";
+  $cmd .= " uaws:www/vhosts/ixnay/htdocs/cams/$name/graphs2";
+
+  print "$cmd\n";
+  #$out = `$cmd`;
+  #print "$out\n";
+
+  $cmd  = "/usr/bin/ssh uaws ";
+  $cmd .= "www/vhosts/ixnay/bin/graphs4.pl";
+
+  print "$cmd\n";
+  #$out = `$cmd`;
+  #print "$out\n";
+}
+
+exit;
+
+#
+#
+#
+
+sub record_dat
+{
+  my($dfile, $data) = @_;
+ 
+  ## check times for potential gap
+  my $mtime = (stat($dfile))[9];
+  my $gap;
+  if(($mtime - $time) > 3600) {
+    $gap = "\n";
+  }
+
+  ## write
+  open(NET, ">>$dfile");
+  print NET $gap, "$date $net\n";
+  close(NET);
+
+  ## archive
+  $dfile =~ /(.*)\.txt/;
+  my $dir = $1;
+  `/bin/cp $dfile $dir/$file`;
+}
+

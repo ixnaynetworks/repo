307ad276aee6f42f8e84ba9c4c7e428ca23df34c
Michael Hansen <michael@han5en.org>
2016-09-27 13:19:24 -0600

    add power cycle script

---
+++ bin/power_cycle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import sys
+import RPi.GPIO as GPIO
+import time
+
+pin = sys.argv[0]
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(pin, GPIO.OUT) 
+time.sleep(2); 
+GPIO.output(pin, GPIO.HIGH)
+
+# time to sleep between operations in the main loop
+
+try:
+  GPIO.output(pin, GPIO.LOW)
+  print "POWER CYCLE..."
+  time.sleep(2); 
+  GPIO.cleanup()
+  print "Good bye!"
+
+# End program cleanly with keyboard
+except KeyboardInterrupt:
+  print "  Quit"
+
+  # Reset GPIO settings
+  GPIO.cleanup()
+

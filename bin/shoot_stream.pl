9865eb9bcbd89550bd7f0c63e427347f14662294
Michael <michael@ixnay.net>
2017-01-12 15:24:51 -0700

    will try multiple times to restart raspivid

--- bin/shoot_stream.pl
+++ bin/shoot_stream.pl
@@ -7,15 +7,10 @@ my $key = $ARGV[1];
 # am i already running?
 #
 
-my $cmd = "/bin/ps auxww | /bin/grep -v grep | /bin/grep perl | /bin/grep shoot";
-print "cmd=$cmd\n";
-my $out = `$cmd`;
-print "$out\n";
-print "\n";
-
-my @proc = split(/\n/, $out);
-if($#proc > 0) {
-  die "another shoot.pl is already running!";
+my $pid = `/usr/bin/pgrep shoot.pl`;
+if($pid) {
+  chomp($pid);
+  die "another shoot.pl ($pid) is already running!";
 }
 
 #
@@ -33,20 +28,13 @@ print "\n";
 # stop stream?
 #
 
-my $cmd = "/bin/ps -ef | /bin/grep -v grep | /bin/grep ffmpeg";
-print "cmd=$cmd\n";
-my $out = `$cmd`;
-print "$out\n";
-print "\n";
-
-#my @proc = split(/\n/, $out);
-#if($#proc > -1) {
-if($out =~ /(\d+) /) {
-  print "ffmpeg is running!";
-  print "pid=$1\n";
-  `/bin/kill $1`;
+my $pid = `/usr/bin/pgrep ffmpeg`;
+if($pid) {
+  chomp($pid);
+  print "ffmpeg is running ($pid)!";
+  `/bin/kill $pid`;
   ## sleep here because if no process was found it is unnecessary
-  sleep 3;
+  sleep 1;
 }
 
 #
@@ -66,9 +54,24 @@ print "\n";
 
 ##raspivid -o - -t 0 -vf -hf -fps 30 -b 6000000 -rot 90 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/amz7-tkk8-fdek-8hhw
 
-sleep 3;
-my $cmd = "/usr/bin/raspivid -o - -t 0 -vf -hf -fps 30 -b 25000000 -rot 90 -w 1280 -h 720 | /usr/local/bin/ffmpeg -loglevel panic -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/$key";
-system("$cmd &");
+my $try;
+while($try <= 3) {
+  $try++;
+  sleep 1;
+
+  my $cmd = "/usr/bin/raspivid -o - -t 0 -vf -hf -fps 30 -b 25000000 -rot 90 -w 1280 -h 720 | /usr/local/bin/ffmpeg -loglevel panic -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/$key";
+  system("$cmd &");
+
+  my $pid = `/usr/bin/pgrep raspivid`;
+  if($pid) {
+    chomp($pid);
+    print "\nraspivid is running ($pid)...\n\n";
+    last;
+  }
+  else {
+    print "\nraspivid is NOT running, will try to start it ", (3 - $try), " more times...\n\n";
+  }
+}
 
 #
 # upload filename to ixnay, and a dir computed from the hostname
@@ -98,4 +101,10 @@ print "cmd=$cmd\n";
 my $out = `$cmd`;
 print "out=$out\n";
 print "\n";
+
+#
+#
+#
+
+exit;
 

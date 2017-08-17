505063b3e63437146d5d96627dc208d031090e77
Michael <michael@ixnay.net>
2017-08-17 14:51:45 -0600

    rotation support

--- bin/shoot_stream.pl
+++ bin/shoot_stream.pl
@@ -77,8 +77,11 @@ print "\n";
 
 ##raspivid -o - -t 0 -vf -hf -fps 30 -b 6000000 -rot 90 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/amz7-tkk8-fdek-8hhw
 
-$args =~ /\-rot(?:ation)? (\d+)/;
-my $rot = $1;
+#$args =~ /\-rot(?:ation)? (\d+)/;
+#my $rot = $1;
+## not sure why this has to be 270...
+## maybe just opposite of the still shots?!
+my $rot = 270;
 
 my $try;
 while($try <= 3) {

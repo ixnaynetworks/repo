c8d9025a77730ba595d9a59af7beebca9227e686
Michael <michael@ixnay.net>
2017-10-17 15:47:09 -0600

    check interfaces and reboot

--- bin/heal_network.pl
+++ bin/heal_network.pl
@@ -1,27 +1,95 @@
 #!/usr/bin/perl
 
-## test connection to uaws2
-## if yes
-##   exit
+my @ip = split(/\s+/, `/sbin/ip route | /bin/grep default`);
+my $gw = $ip[2];
+my $dev = $ip[4];
 
-## test connection to gateway
-## if yes
-##   exit
+print "gw=$gw\n";
+print "dev=$dev\n";
 
-## restart my interface
-## down; sleep; up
+#
+# ping gateway
+#
 
-## test connection to uaws2
-## if yes
-##   exit
+if(&ping($gw)) {
+  print "\nnetwork is up\n";
+  exit;
+}
 
-## test connection to gateway
-## if yes
-##   exit
+#
+# try again
+#
 
-## have i rebooted in the last some timeframe?
-## if yes
-##   exit
+sleep 5;
+if(&ping($gw)) {
+  print "\nnetwork is up\n";
+  exit;
+}
 
-## reboot
+#
+# restart my interface
+#
+
+print "\nrestarting network...\n";
+&cmd("/sbin/ifdown --force $dev");
+sleep 10;
+&cmd("/sbin/ifup $dev > /dev/null 2>&1");
+sleep 10;
+
+if(&ping($gw)) {
+  print "successful networking restart\n";
+  exit;
+}
+
+#
+# did i reboot in the past hour?
+#
+
+my $uptime = &cmd("/bin/cat /proc/uptime");
+$uptime =~ /(\d+)\./;
+my $sec = $1;
+print "sec=$sec\n";
+if($sec < 3600) {
+  print "\nalready rebooted in the past hour\n";
+  exit;
+}
+
+#
+# reboot
+#
+
+print "\nrebooting...\n";
+exit;
+`/sbin/reboot`;
+
+exit;
+
+#
+#
+#
+
+sub ping
+{
+  my($addr) = @_;
+
+  my $ping = &cmd("/bin/ping -c4 -w1 $addr");
+  if($ping =~ /1 received/s) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+sub cmd
+{
+  my($cmd) = @_;
+
+  print "\ncmd=$cmd\n";
+  my $out = `$cmd`;
+  print "out=$out\n";
+  
+  return $out;
+}
+
 

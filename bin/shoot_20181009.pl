3d1c5ff2b2465ff8184f7d423316f4795767b967
michael <michael@ixnay.net>
2018-10-09 18:02:49 -0600

    upped bitrate

--- bin/shoot_20181009.pl
+++ bin/shoot_20181009.pl
@@ -179,7 +179,7 @@ sub stream_start
 
     ## leaving off the ffmpeg path, because now i'm getting one from the OS?...
 
-    my $cmd = "/usr/bin/raspivid -o - -t 0 -vf -hf -b 1500000 -fps 30 -rot $rot -w 1280 -h 720 -a 1036 | ffmpeg -loglevel panic -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/$key";
+    my $cmd = "/usr/bin/raspivid -o - -t 0 -vf -hf -b 3000000 -fps 30 -rot $rot -w 1280 -h 720 -a 1036 | ffmpeg -loglevel panic -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/$key";
 
     print "$cmd\n";
     system("$cmd &");

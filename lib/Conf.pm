b25a431edded756a1d80ed41cc1f24155ded0443
Michael <michael@ixnay.net>
2019-02-28 19:05:44 -0700

    moved format to Conf and added temp stuff to Conf

--- lib/Conf.pm
+++ lib/Conf.pm
@@ -242,5 +242,133 @@ sub video_logo_pos
   return $self->{'video_logo_pos'};
 }
 
+#
+#
+#
+
+sub format
+{
+  my($self, $arg, $w, $h) = @_;
+
+  $arg =~ /(.*)\/(.*)\/(.*)/;
+print "arg=$arg\n";
+  ## do i care?
+  my $cam = $1;
+  my $img = $3;
+  my $base = "/home/pi";
+  my $file = $base . "/$2/$img";
+
+  unless($cam and $img and -e $file) {
+    die "no such file: $file";
+  }
+
+  #
+  # logo stuff
+  #
+
+  my $logo_pos = "northwest";
+  if(-e "$base/conf/logo_northeast") {
+    $logo_pos = "northeast";
+  }
+  elsif(-e "$base/etc/logo_northeast") {
+    $logo_pos = "northeast";
+  }
+
+  my $offset = 0;
+  if($logo_pos eq "northeast") {
+    $offset = $w - 200;
+  }
+
+  #
+  # pick day/night logo
+  #
+
+  ## convert raw/20180413204005.jpg[200x200+1080+0] -colorspace Gray -format "%[fx:image.mean]" info:
+  my $brightness_cmd = "/usr/bin/convert $file\[200x200+$offset+0\] -colorspace Gray -format \"%[fx:image.mean]\" info:";
+  print "\n\n$brightness_cmd";
+  my $brightness = `$brightness_cmd`;
+  chomp($brightness);
+  print "\n$brightness";
+
+  my $logo;
+  mkdir "$base/vals" unless(-e "$base/vals");
+  if($brightness > 0.3) {
+    $logo = "$base/conf/logo_day.png";
+    `/bin/rm $base/vals/bg_dark ; /usr/bin/touch $base/vals/bg_bright`;
+  }
+  else {
+    $logo = "$base/conf/logo_night.png";
+    `/bin/rm $base/vals/bg_bright ; /usr/bin/touch $base/vals/bg_dark`;
+  }
+  print "\n\nlogo=$logo";
+
+  #
+  # caption stuff
+  #
+
+  my $label = $cam;
+  if(-e "$base/conf/caption") {
+    my $caption = `/bin/cat $base/conf/caption`;
+    chomp $caption;
+    $label = $caption;
+  }
+  print "\n";
+
+  if(-e "/home/pi/temp.txt")
+  {
+    open(TEMP, "/home/pi/temp.txt");
+    $temp = <TEMP>;
+    close(TEMP);
+    chomp($temp);
+
+    if($temp) {
+      $label .= " // $temp" . "°f";
+    }
+  }
+
+  #
+  # compute dimensions of bottom bar
+  #
+
+  ## 720 - 45 = 675
+  ## 720 - 15 = 705
+  my $rectangle = "0," . ($h - 45) . ",$w,$h";
+  my $baseline = $h - 15;
+
+  #
+  # first convert
+  #
+
+  mkdir("$base/tmp") unless(-e "$base/tmp");
+
+  my $cmd = "/usr/bin/convert $file -fill '#000A' -draw 'rectangle $rectangle' -fill '#CCCCCC' -pointsize 20 -font Courier-Bold -annotate +15+$baseline '$label'";
+  if($logo) {
+    $cmd .= " $logo -gravity $logo_pos -composite";
+  }
+  $cmd .= " $base/conf/rpi.png -gravity southeast -geometry +135+10 -composite $base/tmp/left_bottom_label.jpg";
+  print "cmd=$cmd\n";
+  $out = `$cmd`;
+  print "out=$out\n";
+  print "\n";
+
+  #
+  # compute timestamp
+  # 
+
+  $img =~ /(\d{4})(\d\d)(\d\d)(\d\d)(\d\d)/;
+  my $timestamp = "$1/$2/$3 $4:$5";
+
+  #
+  # final processing
+  #
+
+  mkdir("$base/arc") unless(-e "$base/arc");
+  my $cmd = "/usr/bin/convert -background '#00000000' -fill '#CCCCCC' -pointsize 20 -font Courier-Bold label:'$timestamp    ixnay.net' miff:- | composite -gravity southeast -geometry +15+9 - $base/tmp/left_bottom_label.jpg $base/arc/$img";
+  print "cmd=$cmd\n";
+  $out = `$cmd`;
+  print "out=$out\n";
+  print "\n";
+}
+
 1;
 

467225526f69c893c3c3e8bd3536280229b9b37e
Michael <michael@ixnay.net>
2019-02-28 19:07:39 -0700

    grab temp from cherrypeak web site

---
+++ bin/temp_cp.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+my $cmd = "/usr/bin/curl -s 'http://www.skicherrypeak.com/rpt/?snow-report'";
+#print "$cmd\n";
+my $out = `$cmd`;
+#print "$out\n";
+
+my($station) = @ARGV;
+if($out =~ /$station.*?Air Temperature: <\/span><span class="Normal_text xr_s8" style="">(\d\d)\./si) {
+  open(FILE, ">/home/pi/temp.txt");
+  print FILE $1;
+  close(FILE);
+}
+
